d48e11ff8ea0aa9b1391f0a9051d9a5949d0781f
Dmitry Shurco <dmitry@werbot.com>
2023-03-02 21:15:22 +0000

    🎈 perf: Component name renamed

--- src/components/Select/index.vue
+++ src/components/Select/index.vue
@@ -79,12 +79,12 @@ const setLanguage = (val: string) => {
 
 .form-dropdown {
   button {
-    @apply min-w-[8rem] justify-between inline-block rounded border border-solid border-gray-300 px-2 py-2 leading-tight;
+    @apply min-w-[10rem] justify-between inline-block rounded border border-solid border-gray-300 px-2 py-2 leading-tight;
     @apply flex items-center whitespace-nowrap;
   }
 
   ul {
-    @apply absolute z-10 mt-2 min-w-[8rem] rounded bg-gray-50 ring-1 ring-gray-300;
+    @apply absolute z-10 mt-2 min-w-[10rem] rounded bg-gray-50 ring-1 ring-gray-300;
 
     li {
       @apply cursor-pointer select-none p-2 hover:bg-gray-200;
